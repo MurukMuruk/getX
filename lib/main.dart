ef21554289df6578ce04b8b3c70504f5bde95804
최호연 <hoyun0110@dimigo.hs.kr>
2022-05-19 20:07:13 +0900

    sss

---
+++ lib/main.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+import 'package:untitled1/src/first.dart';
+
+void main(){
+  runApp(const MaterialApp(home: MyApp(),));
+}
+
+class MyApp extends StatefulWidget
+{
+  const MyApp({Key? key}) : super(key: key);
+  static const color = const Color(0xff42f551);
+
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  _MyAppState createState()=> _MyAppState();
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+
+     children: [
+       Text('Hello, World!', style: TextStyle(fontSize: 30),),
+        Container(
+          width: 50,
+          height: 50,
+          color: Colors.yellow,
+        ),
+       SizedBox(height: 50,),
+       Container(
+         width: 50,
+         height: 50,
+         color: Colors.blue,
+       ),
+       SizedBox(height: 50,),
+
+
+       Row(
+         mainAxisAlignment: MainAxisAlignment.center,
+         children: [
+           Container(
+             width: 50,
+             height: 50,
+             color: Colors.lightGreenAccent,
+           ),
+           SizedBox(width: 50,),
+           Container(
+             width: 50,
+             height: 50,
+             color: Colors.red,
+           ),
+           SizedBox(width: 50,),
+           Container(
+             width: 50,
+             height: 50,
+             color: Colors.indigo,
+           )
+         ],
+       ),
+       Container(
+         child: Text("Hi Flutter!", style: TextStyle(fontSize: 30),),
+       ),
+
+       SizedBox(height: 50,),
+
+        Container(
+          child: Text("My Profile:", style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),),
+        ),
+
+
+          SizedBox(height: 50,),
+
+       Row(
+         mainAxisAlignment: MainAxisAlignment.center,
+         children: [
+           Container(
+             width: 150, height: 150,
+             color: Colors.amber,
+           ),
+            SizedBox(width: 50,),
+         Column(
+           children: [
+             Text('Name: choi Ho Yeon'),
+             Text('ID: @c.0h_y'),
+             Text('Age: 17'),
+             Text('Hobby: ___'),
+             Text('Favorite food: ___'),
+           ],
+         )
+         ],
+       ),
+
+         SizedBox(height: 50,), 
+       RaisedButton(child: Text("Go to main page"),
+
+           onPressed:()
+       {
+
+         Navigator.push(context, MaterialPageRoute(builder: (context)=>first()));
+       }
+       )
+     ],
+        ),
+      ),
+    );
+  }
+}
+
+

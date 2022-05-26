19fef8c35a6f75834c33099970d16f768afdac44
최호연 <hoyun0110@dimigo.hs.kr>
2022-05-26 12:15:41 +0900

    init code

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,120 @@
+import 'package:get/get.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:untitled1/src/first.dart';
 
-void main(){
-  runApp(const MaterialApp(home: MyApp(),));
-}
+void main() => runApp(app());
 
-class MyApp extends StatefulWidget
-{
-  const MyApp({Key? key}) : super(key: key);
-  static const color = const Color(0xff42f551);
+class app extends StatelessWidget {
+  const app({Key? key}) : super(key: key);
 
 
   @override
-  State<MyApp> createState() => _MyAppState();
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      home: good(),
+      initialRoute: "/",
+      getPages: [
+        GetPage(name: '/', page: ()=> const home()),
+      ],
+    );
+
+  }
 }
 
-class _MyAppState extends State<MyApp> {
-  @override
-  _MyAppState createState()=> _MyAppState();
 
+class good extends StatelessWidget {
+  const good({Key? key}) : super(key: key);
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
 
-     children: [
-       Text('Hello, World!', style: TextStyle(fontSize: 30),),
-        Container(
-          width: 50,
-          height: 50,
-          color: Colors.yellow,
-        ),
-       SizedBox(height: 50,),
-       Container(
-         width: 50,
-         height: 50,
-         color: Colors.blue,
-       ),
-       SizedBox(height: 50,),
+          children: [
+            Text('Hello, World!', style: TextStyle(fontSize: 30),),
+            Container(
+              width: 50,
+              height: 50,
+              color: Colors.yellow,
+            ),
+            SizedBox(height: 50,),
+            Container(
+              width: 50,
+              height: 50,
+              color: Colors.blue,
+            ),
+            SizedBox(height: 50,),
 
 
-       Row(
-         mainAxisAlignment: MainAxisAlignment.center,
-         children: [
-           Container(
-             width: 50,
-             height: 50,
-             color: Colors.lightGreenAccent,
-           ),
-           SizedBox(width: 50,),
-           Container(
-             width: 50,
-             height: 50,
-             color: Colors.red,
-           ),
-           SizedBox(width: 50,),
-           Container(
-             width: 50,
-             height: 50,
-             color: Colors.indigo,
-           )
-         ],
-       ),
-       Container(
-         child: Text("Hi Flutter!", style: TextStyle(fontSize: 30),),
-       ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: 50,
+                  height: 50,
+                  color: Colors.lightGreenAccent,
+                ),
+                SizedBox(width: 50,),
+                Container(
+                  width: 50,
+                  height: 50,
+                  color: Colors.red,
+                ),
+                SizedBox(width: 50,),
+                Container(
+                  width: 50,
+                  height: 50,
+                  color: Colors.indigo,
+                )
+              ],
+            ),
+            Container(
+              child: Text("Hi Flutter!", style: TextStyle(fontSize: 30),),
+            ),
 
-       SizedBox(height: 50,),
+            SizedBox(height: 50,),
 
-        Container(
-          child: Text("My Profile:", style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),),
-        ),
+            Container(
+              child: Text("My Profile:",
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),),
+            ),
 
 
-          SizedBox(height: 50,),
+            SizedBox(height: 50,),
 
-       Row(
-         mainAxisAlignment: MainAxisAlignment.center,
-         children: [
-           Container(
-             width: 150, height: 150,
-             color: Colors.amber,
-           ),
-            SizedBox(width: 50,),
-         Column(
-           children: [
-             Text('Name: choi Ho Yeon'),
-             Text('ID: @c.0h_y'),
-             Text('Age: 17'),
-             Text('Hobby: ___'),
-             Text('Favorite food: ___'),
-           ],
-         )
-         ],
-       ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: 150, height: 150,
+                  color: Colors.amber,
+                ),
+                SizedBox(width: 50,),
+                Column(
+                  children: [
+                    Text('Name: choi Ho Yeon'),
+                    Text('ID: @c.0h_y'),
+                    Text('Age: 17'),
+                    Text('Hobby: ___'),
+                    Text('Favorite food: ___'),
+                  ],
+                )
+              ],
+            ),
 
-         SizedBox(height: 50,), 
-       RaisedButton(child: Text("Go to main page"),
-
-           onPressed:()
-       {
-
-         Navigator.push(context, MaterialPageRoute(builder: (context)=>first()));
-       }
-       )
-     ],
+            SizedBox(height: 50,),
+            RaisedButton(child: Text('Go to Page'),
+                onPressed: () {
+                  Get.toNamed('/home');
+                }
+            ),
+          ],
         ),
       ),
     );
   }
 }
-
 
--- lib/src/first.dart
+++ lib/src/first.dart
@@ -1,10 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-class first extends StatelessWidget {
-  const first({Key? key}) : super(key: key);
+class home extends StatelessWidget {
+  const home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return  Scaffold(
+      body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: 70, height: 30,
+                color: Colors.amber,
+              ),
+              Container(
+                width: 70, height: 30,
+                color: Colors.lightGreenAccent,
+
+              ),
+RaisedButton(
+  child: Text('PUSH'),
+    onPressed: (){
+  Get.toNamed("/");
+})
+            ],
+          )
+      ),
+    );
   }
 }
